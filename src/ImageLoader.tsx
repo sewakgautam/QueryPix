6ea63f25813abbbe8432209dcc0284d835713b92
sewakgautam <sewak.gautam58@gmail.com>
2023-10-01 19:54:17 +0545

    Image Loading Optimized

--- src/ImageLoader.tsx
+++ src/ImageLoader.tsx
@@ -7,7 +7,7 @@ export default function ImageLoader(images: any) {
     final = result.data.map((image: any) => {
       return (
         <Image
-          url={image.urls.regular}
+          url={image.urls.small}
           title={image.alt_description}
           raw={image.urls.raw}
         />
@@ -25,16 +25,18 @@ export default function ImageLoader(images: any) {
   }
 
   return (
-    <div style={{
-      display: "grid",
-      gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
-      gap: "50px",
-      placeContent: "center",
-      placeItems: "center",
-      margin: "0 auto",
-      width: "100%",
-      maxWidth: "1200px",
-    }}>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+        gap: "50px",
+        placeContent: "center",
+        placeItems: "center",
+        margin: "0 auto",
+        width: "100%",
+        maxWidth: "1200px",
+      }}
+    >
       {final}
     </div>
   );
