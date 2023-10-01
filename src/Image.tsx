f33810505ea91ec4204eb5b201aa9447e7b51da2
Kishor U. <blackkspydo@gmail.com>
2023-10-01 12:32:56 +0000

    added image hover

--- src/Image.tsx
+++ src/Image.tsx
@@ -6,26 +6,21 @@ const Image = ({
   url: string;
   title: string;
   raw: string;
-}) => (
-  <div>
-    <a href={raw} target="_blank">
-      <img
-        style={{
-          display: "flex",
-          width: 300,
-          height: 300,
-          objectFit: "cover",
-          opacity: 0,
-          transition: "opacity 0.5s ease-in-out",
-        }}
-        src={url}
-        alt={title}
-        onLoad={(e) => {
-          e.currentTarget.style.opacity = "1";
-        }}
-      />
-    </a>
-  </div>
-);
+}) => (<div
+  className="img-card"
+>
+  <a href={ raw } target="_blank"
+  >
+    <img
+      src={ url }
+      alt={ title }
+      onLoad={ (e) => {
+        e.currentTarget.style.opacity = "1";
+      } }
+    />
+  </a>
+</div>);
+
+
 
 export default Image;
